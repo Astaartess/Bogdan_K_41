4dc8d9ca39b15cf62e49c799646d1d92a5d021c1
Bogdan Pistun <pistunbogdan114@gmail.com>
2025-09-14 11:58:54 +0300

    Fix: add JUnit dependency for unit tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ dependencies {
     implementation("androidx.compose.material3:material3:1.2.0")
     implementation("com.google.android.material:material:1.11.0")
 
+    testImplementation("junit:junit:4.13.2")
+
     debugImplementation("androidx.compose.ui:ui-tooling:1.6.0")
     debugImplementation("androidx.compose.ui:ui-tooling-preview:1.6.0")
 }
